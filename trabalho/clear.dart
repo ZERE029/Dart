d7dfa016d7561ffbd13cf25440ecb0cbb6427f2a
ZERE029 <matheusvital029@gmail.com>
2024-05-06 20:21:15 -0300

    att

---
+++ trabalho/clear.dart
@@ -0,0 +1,11 @@
+void main (){
+  List<int> numeros = [1, 2, 3, 4, 5];
+
+  print('Lista antes da limpeza: $numeros');
+
+  // Limpar a lista
+  numeros.clear();
+
+  print('Lista após a limpeza: $numeros'); 
+
+}
--- trabalho/remove.dart
+++ trabalho/remove.dart
@@ -1,7 +1,8 @@
 void main() {
   List<int> numeros = [1, 2, 3, 4, 5];
-
-  var removido = numeros.remove(6); // Remove o número 3 da lista
-  print('O numero que foi removido foi $removido');
-  print(numeros); // Saída: [1, 2, 4, 5]
+    numeros.remove(4);
+    numeros.remove(1); 
+ 
+ 
+  print(numeros); 
 }
